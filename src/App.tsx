e3736e6476d936ccfac355a4fea597b5f87c1021
Patryk Ihas <97395198+ihasp@users.noreply.github.com>
2024-04-15 17:37:44 +0200

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import {TaskPlanning} from "./TaskPlanning";
+
+function App() {
+
+  return (
+    <div className='App'><TaskPlanning/></div>
+  )
+}
+
+export default App
---
+++ src/TaskPlanning.tsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+
+export const TaskPlanning: React.FC = () => {
+  return <div></div>;
+};
