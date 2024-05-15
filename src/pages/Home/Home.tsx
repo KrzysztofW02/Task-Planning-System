f19af9969952203fbd6e62773825204962d9369a
Patryk Ihas <97395198+ihasp@users.noreply.github.com>
2024-05-15 10:15:48 +0200

    Added grid to home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,13 +4,20 @@ function HomeComponent() {
   return (
     <>
       <div className="HomeComponent">
-        <div className="col-sm-10 HomeText">
-          <h1>Witaj,</h1>
-          <br></br>
-          <h2>Zaloguj się, aby przejść dalej</h2>
-        </div>
-        <div className="col-sm-2">
+        <div className="row">
+          <div className="col-sm-8 HomeText">
+            <h1>Witaj,</h1>
+            <br></br>
+            <h2>Zaloguj się, aby przejść dalej</h2>
+          </div>
+          <div className="col-sm-2" style={{marginLeft:'7em'}}>
             <button className="btn-primary btnhomelogin">Zaloguj Się</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-10">
+            <p style={{ color: "white" }}>newinfogrid</p>
+          </div>
         </div>
       </div>
     </>
