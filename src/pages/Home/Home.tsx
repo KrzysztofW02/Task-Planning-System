d5e4a61b426df1990b58b72ac1d7a9283ace54aa
Patryk Ihas <97395198+ihasp@users.noreply.github.com>
2024-05-20 19:31:09 +0200

    Grid fixes, flexbox fixes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,15 +4,15 @@ function HomeComponent() {
   return (
     <>
       <div className="container HomeComponent">
-        <div className="row" style={{display:'flex'}}>
-          <div className="col-sm-8">
+        <div className="row" style={{display:'flex', flexDirection:'column'}}>
+          <div className="col1">
             <div className="HomeText">
               <h1>Witaj,</h1>
               <br></br>
               <h2>Zaloguj się, aby przejść dalej</h2>
             </div>
           </div>
-          <div className="col-sm-4">
+          <div className="col2">
             <button className="btnn">Zaloguj Się</button>
           </div>
         </div>
