3d832e2f20408a094a2b7147c28f8185ac1fd5a2
Patryk Ihas <97395198+ihasp@users.noreply.github.com>
2024-05-24 12:44:23 +0200

    Event page layout

--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -4,11 +4,20 @@ function EventComponent(){
     
     return (
       <>
-        <div className="row">
-          <div className="col1">
-            <p style={{ color: "white" }}>test informacji na stronie</p>
+        <div className="container eventContainer">
+          <div className="row ctc">
+            <div className="col ctc">
+              <h1>Witaj,</h1><br/>
+              <h2>{"nazwa użytkownika"}</h2>
+            </div>
+            <div className="row ctc mt-5">
+              <div className="col ctc">
+                <p>?info?</p>
+              </div>
+              <div className="col ctc">kalendarz</div>
+              <div className="col ctc">wydarzenia</div>
+            </div>
           </div>
-          <div className="col2"></div>
         </div>
       </>
     );
