0d53a5f6af69b47926c0dbd28feb2a0638e399c3
Krzysztof Wasik <Krzysztof.Wasik2002@gmail.com>
2024-05-27 19:29:32 +0200

    Refactor login and registration pages added login and registration axios

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
@@ -6,36 +8,75 @@ interface LoginPageProps {
 }
 
 function LoginPage({ onRegisterClick }: LoginPageProps) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/Authentication/Login",
+        {
+          username,
+          password,
+        }
+      );
+
+      if (response.status === 200) {
+        console.log("Login successful");
+        setIsLoggedIn(true);
+      }
+    } catch (error) {
+      console.error("Error during login", error);
+    }
+  };
+
+  if (isLoggedIn) {
+    return <p>Udało się zalogować!</p>;
+  }
+
   return (
     <div className="login-container">
       <div className="login-form">
-        <>
-          <Form.Label htmlFor="inputPassword5">Login</Form.Label>
-          <Form.Control
-            placeholder="Recipient's username"
-            aria-label="Recipient's username"
-            aria-describedby="basic-addon2"
-          />
-        </>
-        <br />
-        <>
-          <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-          <Form.Control
-            type="password"
-            id="inputPassword5"
-            aria-describedby="passwordHelpBlock"
-            placeholder="Password's username"
-          />
-          <Form.Text id="passwordHelpBlock" muted>
-            Your password must be 8-20 characters long, contain letters and
-            numbers, and must not contain spaces, special characters, or emoji.
-          </Form.Text>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Login</Form.Label>
+            <Form.Control
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Recipient's username"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+          </Form.Group>
+          <br />
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="inputPassword5"
+              aria-describedby="passwordHelpBlock"
+              placeholder="Password's username"
+            />
+            <Form.Text id="passwordHelpBlock" muted>
+              Your password must be 8-20 characters long, contain letters and
+              numbers, and must not contain spaces, special characters, or
+              emoji.
+            </Form.Text>
+          </Form.Group>
           <br />
           <Button variant="primary" type="submit">
             Login
           </Button>
-          <button onClick={onRegisterClick}>Register</button>
-        </>
+          <button type="button" onClick={onRegisterClick}>
+            Register
+          </button>
+        </Form>
       </div>
     </div>
   );
